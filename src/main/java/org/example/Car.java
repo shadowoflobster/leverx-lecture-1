74647db0059f785eb89855fb856e9d4d659b7f42
shadowoflobster <guraspishvilileqso@gmail.com>
2025-10-23 17:41:46 +0400

    Feat: Added Car class and usage in main

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,51 @@
+package org.example;
+
+public class Car {
+    private String name;
+    private int horsePower;
+    private int weight;
+    private float fuelLevel;
+    private float fuelConsumption;
+
+    public Car(String name, int horsePower, int weight, int fuelLevel){
+        this.name=name;
+        this.horsePower=horsePower;
+        this.weight=weight;
+        this.fuelLevel=fuelLevel;
+        calculateFuelConsumption();
+    }
+
+    public void calculateFuelConsumption(){
+        this.fuelConsumption = (float) ((horsePower / 2f) + (weight / 2f))/100;
+    }
+
+    public void drive(int distance){
+        float requiredFuel = (float) distance / 100f * fuelConsumption;
+        if(requiredFuel < fuelLevel){
+            fuelLevel -= requiredFuel;
+            System.out.println("You drove "+distance+"KM, "+fuelLevel+" Liters of fuel remaining.");
+        }else System.out.println("You don't have enough fuel. (Required: "+ requiredFuel+"L, your fuel level: "+fuelLevel+"L)");
+    }
+
+    public void refuel(float fuel){
+        this.fuelLevel+=fuel;
+        System.out.println("You added "+fuel+"L fuel");
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getHorsePower() {
+        return horsePower;
+    }
+    public int getWeight(){
+        return weight;
+    }
+    public float getFuelConsumption(){
+        return fuelConsumption;
+    }
+    public void setName(String  name){
+        this.name=name;
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+
 public class Main {
     public static void main(String args[]){
         System.out.println("Hello LeverX");
+
+        Car mercedes = new Car("Mercedes", 250,1800,30);
+        mercedes.drive(100);
+        mercedes.drive(20);
+        mercedes.drive(200);
+        mercedes.refuel(30);
+        mercedes.drive(200);
     }
 }
