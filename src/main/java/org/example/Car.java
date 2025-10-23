13e9968ba2f488d7474431f3a1d9fd05683460bf
shadowoflobster <guraspishvilileqso@gmail.com>
2025-10-23 18:42:24 +0400

    Add Car class which uses lombok for getters and setters

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class Car {
+    @Setter
+    private String name;
+    private final int horsePower;
+    private final int weight;
+    private float fuelLevel;
+    private float fuelConsumption;
+
+    public Car(String name, int horsePower, int weight, int fuelLevel){
+        this.name=name;
+        this.horsePower=horsePower;
+        this.weight=weight;
+        this.fuelLevel=fuelLevel;
+        calculateFuelConsumption();
+    }
+
+    public void calculateFuelConsumption(){
+        this.fuelConsumption = ((horsePower / 2f) + (weight / 2f))/100;
+    }
+
+    public void drive(int distance){
+        float requiredFuel = (float) distance / 100f * fuelConsumption;
+        if(requiredFuel < fuelLevel){
+            fuelLevel -= requiredFuel;
+            System.out.println("You drove "+distance+"KM, "+fuelLevel+" Liters of fuel remaining.");
+        }else System.out.println("You don't have enough fuel. (Required: "+ requiredFuel+"L, your fuel level: "+fuelLevel+"L)");
+    }
+
+    public void refuel(float fuel){
+        this.fuelLevel+=fuel;
+        System.out.println("You added "+fuel+"L fuel");
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println("Hello LeverX");
+
+
+        Car mercedes = new Car("Mercedes", 250,1800,30);
+        mercedes.drive(100);
+        mercedes.drive(20);
+        mercedes.drive(200);
+        mercedes.refuel(30);
+        mercedes.drive(200);
+        System.out.println("name of this car is: "+mercedes.getName());
     }
 }
