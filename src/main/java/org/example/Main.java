4a1557caa14f590844764d27482e7426129fc36f
shadowoflobster <guraspishvilileqso@gmail.com>
2025-10-24 12:28:21 +0400

    added some usage of car class methods for testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,15 @@ public class Main {
 
 
         Car mercedes = new Car("Mercedes", 250,1800,30);
+        Car.Engine engine = mercedes.new Engine();
+        Car.Manual manual = new Car.Manual();
+        manual.print();
+        engine.start();
+        engine.start();
         mercedes.drive(100);
         mercedes.drive(20);
+        engine.stop();
+        engine.stop();
         mercedes.drive(200);
         mercedes.refuel(30);
         mercedes.drive(200);
