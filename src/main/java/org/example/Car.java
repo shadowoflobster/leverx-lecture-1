3bb9cbb1da704748a7de2db2cc63d12df5d085ff
shadowoflobster <guraspishvilileqso@gmail.com>
2025-10-24 12:13:33 +0400

    feat: add engine running checks before driving and refuel

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -13,49 +13,53 @@ public class Car {
     private float fuelConsumption;
     private boolean engineRunning = false;
 
-    public Car(String name, int horsePower, int weight, int fuelLevel){
-        this.name=name;
-        this.horsePower=horsePower;
-        this.weight=weight;
-        this.fuelLevel=fuelLevel;
+    public Car(String name, int horsePower, int weight, int fuelLevel) {
+        this.name = name;
+        this.horsePower = horsePower;
+        this.weight = weight;
+        this.fuelLevel = fuelLevel;
         calculateFuelConsumption();
     }
 
-    public void calculateFuelConsumption(){
-        this.fuelConsumption = ((horsePower / 2f) + (weight / 2f))/100;
+    public void calculateFuelConsumption() {
+        this.fuelConsumption = ((horsePower / 2f) + (weight / 2f)) / 100;
     }
 
     //Inner non-static class
-    public class Engine{
-        public void start(){
-            if(!engineRunning){
+    public class Engine {
+        public void start() {
+            if (!engineRunning) {
                 engineRunning = true;
                 System.out.println("Engine started running");
-            }else
+            } else
                 System.out.println("Engine is already running");
         }
 
-        public void stop(){
-            if(engineRunning){
-               engineRunning = false;
+        public void stop() {
+            if (engineRunning) {
+                engineRunning = false;
                 System.out.println("Engine stopped running");
-            }else
+            } else
                 System.out.println("Engine is already stopped");
         }
     }
 
 
-
-    public void drive(int distance){
-        float requiredFuel = (float) distance / 100f * fuelConsumption;
-        if(requiredFuel < fuelLevel){
-            fuelLevel -= requiredFuel;
-            System.out.println("You drove "+distance+"KM, "+fuelLevel+" Liters of fuel remaining.");
-        }else System.out.println("You don't have enough fuel. (Required: "+ requiredFuel+"L, your fuel level: "+fuelLevel+"L)");
+    public void drive(int distance) {
+        if (engineRunning) {
+            float requiredFuel = (float) distance / 100f * fuelConsumption;
+            if (requiredFuel < fuelLevel) {
+                fuelLevel -= requiredFuel;
+                System.out.println("You drove " + distance + "KM, " + fuelLevel + " Liters of fuel remaining.");
+            } else
+                System.out.println("You don't have enough fuel. (Required: " + requiredFuel + "L, your fuel level: " + fuelLevel + "L)");
+        } else System.out.println("Engine must be running to drive car");
     }
 
-    public void refuel(float fuel){
-        this.fuelLevel+=fuel;
-        System.out.println("You added "+fuel+"L fuel");
+    public void refuel(float fuel) {
+        if (!engineRunning) {
+            this.fuelLevel += fuel;
+            System.out.println("You added " + fuel + "L fuel");
+        } else System.out.println("Please turn off your engine before refueling");
     }
 }
