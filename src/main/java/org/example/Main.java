3a2a3159701eb15bff6affc4f1a5b3a240f7aae6
shadowoflobster <guraspishvilileqso@gmail.com>
2025-10-26 17:07:24 +0400

    Add manual interface

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,27 +3,28 @@ package org.example;
 import java.util.ArrayList;
 import java.util.function.Function;
 
+
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello LeverX");
 
-        Car mercedes = new Car("Mercedes", 250,1800,300);
-        Car.Engine mercedesEngine = mercedes.new Engine();
-        Car.Manual manual = new Car.Manual();
+        Car mercedes = new Car("Mercedes", 250, 1800, 300);
 
-        manual.print();
-        mercedesEngine.start();
+        mercedes.manual.print();
+
+        mercedes.engine.start();
         mercedes.drive(100);
         mercedes.drive(20);
-        mercedesEngine.stop();
-        mercedesEngine.stop();
+        mercedes.engine.stop();
+        mercedes.engine.stop();
         mercedes.drive(200);
         mercedes.refuel(30);
         mercedes.drive(200);
-        System.out.println("name of this car is: "+mercedes.getName());
+        System.out.println("name of this car is: " + mercedes.getName());
 
-        Car bmw = new Car("BMW", 230, 1700,250);
-        Car.Engine bmwEngine = bmw.new Engine();
+        Car bmw = new Car("BMW", 230, 1700, 250);
+
 
         ArrayList<Car> carList = new ArrayList<>();
         carList.add(mercedes);
@@ -32,7 +33,7 @@ public class Main {
 
         /* Anonymous classs implementing function interface
          it will race cars from a list and returns the fastest one */
-        Function<ArrayList<Car>,Car> race = cars -> {
+        Function<ArrayList<Car>, Car> race = cars -> {
             Car fastest = cars.get(0);
             for (Car c : cars) {
                 if (c.getHorsePower() > fastest.getHorsePower())
