56fc48ca96a4db9a5659e34a9114241e830062f2
shadowoflobster <guraspishvilileqso@gmail.com>
2025-10-29 14:23:14 +0400

    formated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.function.Function;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello LeverX");
 
-        Car mercedes = new Car("Mercedes", 250,1800,300);
+        Car mercedes = new Car("Mercedes", 250, 1800, 300);
         Car.Engine mercedesEngine = mercedes.new Engine();
         Car.Manual manual = new Car.Manual();
 
@@ -20,9 +20,9 @@ public class Main {
         mercedes.drive(200);
         mercedes.refuel(30);
         mercedes.drive(200);
-        System.out.println("name of this car is: "+mercedes.getName());
+        System.out.println("name of this car is: " + mercedes.getName());
 
-        Car bmw = new Car("BMW", 230, 1700,250);
+        Car bmw = new Car("BMW", 230, 1700, 250);
         Car.Engine bmwEngine = bmw.new Engine();
 
         ArrayList<Car> carList = new ArrayList<>();
@@ -32,7 +32,7 @@ public class Main {
 
         /* Anonymous classs implementing function interface
          it will race cars from a list and returns the fastest one */
-        Function<ArrayList<Car>,Car> race = cars -> {
+        Function<ArrayList<Car>, Car> race = cars -> {
             Car fastest = cars.get(0);
             for (Car c : cars) {
                 if (c.getHorsePower() > fastest.getHorsePower())
